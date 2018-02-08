0fd59d07cd513073ad357afa295975ac3373b077
kysin han <hanxun@kysintek.com>
2018-02-08 14:31:29 +0800

    fisrt commit.

---
+++ widget.cpp
@@ -0,0 +1,31 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include <QDialog>
+#include <QMessageBox>
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+
+void Widget::keyPressEvent(QKeyEvent *k_event)
+{
+    if(k_event->key()!=16777220)
+    {
+       str.append(k_event->text());
+    }
+    else
+    {
+        ui->label->setText(str);
+       str.clear();
+    }
+
+}
---
+++ widget.h
@@ -0,0 +1,28 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QKeyEvent>
+#include <QDebug>
+
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = 0);
+    ~Widget();
+protected:
+  void keyPressEvent(QKeyEvent *);
+
+private:
+    Ui::Widget *ui;
+    QString str;
+};
+
+#endif // WIDGET_H
